5183a548a7ffd9270b7fe95e4e875d8410be5bd6
sejeongit <chocofondue725@gmail.com>
2021-07-16 09:33:39 +0900

    set_rwd_premier_twin.html and change some img

---
+++ js/src/main.js
@@ -0,0 +1,22 @@
+// js/src/main.js
+
+
+(function($){
+
+
+
+  // 변수 선언
+  var artViewBox = $('#artViewBox');
+  var artViewArea = artViewBox.children('.art_view_area');
+  var timed = 500;
+  var n = 1;
+
+  // setInterval(function(n){
+  //   artViewArea[n].fadeOut();
+  //   artViewArea[n+1].fadeIn();
+  // },timed);
+
+
+
+
+})(jQuery);

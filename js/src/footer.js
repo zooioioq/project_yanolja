cba761e3a4839ca1270b314a26f3c13942e1ecd0
zooioioq <zooi.oi.oq@gmail.com>
2021-07-24 01:58:23 +0900

    피드백 수정 : 320,768 footer 디자인 및 lotte_aquarium.html 내부의 ticket 영역

---
+++ js/src/footer.js
@@ -0,0 +1,17 @@
+// js/src/footer.js
+
+
+(function($){
+
+  // var detailBtnArea = $('.foot_detail_btn');
+  // var detailBtn = detailBtnArea.children('button');
+  // var footLink = $('.foot_link');
+
+
+  // detailBtn.on('click focus', function(e){
+  //   e.preventDefault();
+  //   footLink.css({dsiplay:'none'});
+  // })
+
+
+})(jQuery);
--- js/src/main.js
+++ js/src/main.js
@@ -4,55 +4,61 @@
 (function($){
 
 
-var locationList = $('.recom_location_list');
-var locationLi = locationList.children('li');
-var locationBtn = locationLi.children('button');
-var hotelList = $('.recom_hotel_list');
-var hotelSelect
+  // #recommendBox tab menu 설정
+  var locationList = $('.recom_location_list');
+  var locationLi = locationList.children('li');
+  var locationBtn = locationLi.children('button');
+  var hotelList = $('.recom_hotel_list');
 
 
-hotelList.eq(1).css({display : 'none'});
-hotelList.eq(2).css({display : 'none'});
-hotelList.eq(3).css({display : 'none'});
+  hotelList.eq(1).css({display : 'none'});
+  hotelList.eq(2).css({display : 'none'});
+  hotelList.eq(3).css({display : 'none'});
 
 
-locationLi.eq(0).addClass('act');
-hotelList.attr('tabindex',-1);
-
-locationBtn.on('focus click',function(e){
-  e.preventDefault();
-
-  locationLi.eq(0).removeClass('act');
-
-  var _this = $(this);
-  var _thisParent = _this.parent('li');
-  var _i = _thisParent.index();
-  var _locationEq = locationLi.eq(_i);
-  _locationEq.addClass('act');
-  _locationEq.siblings().removeClass('act');
-  var _hotelEq = hotelList.eq(_i);
-
-  hotelList.attr('tabindex',0);
-  _hotelEq.show();
-  _hotelEq.siblings().hide();
-
-});
+  locationLi.eq(0).addClass('act');
+  hotelList.attr('tabindex',-1);
 
 
+  locationBtn.on('focus click',function(e){
+    e.preventDefault();
+    locationLi.eq(0).removeClass('act');
 
-// locationBtn.on('click focus',function(e){
-//   e.preventDefault();
+    var _this = $(this);
+    var _thisParent = _this.parent('li');
+    var _i = _thisParent.index();
+    var _locationEq = locationLi.eq(_i);
+    var _hotelEq = hotelList.eq(_i);
 
-//   var _this = $(this);
-//   var _thisParent = _this.parent('li');
-//   var _i = _thisParent.index();
+    _locationEq.addClass('act');
+    _locationEq.siblings().removeClass('act');
+    hotelList.attr('tabindex',0);
+    _hotelEq.show();
+    _hotelEq.siblings().hide();
+  });
 
-//   var _hotelEq = hotelList.eq(_i);
-//   hotelSelect = _hotelEq;
 
-//   _hotelEq.show();
-//   _hotelEq.siblings('hotelList').hide();
-// });
+
+
+
+  // // #artViewBox fade banner 설정
+  // var artViewBox = $('#artViewBox');
+  // var artViewArea = artViewBox.children('.art_view_area');
+  // var newCount = 0;
+  // var beforeCount = newCount;
+  // var timed = 500;
+
+  // var slideFadeFn = function(newCount){
+  //   artViewArea.eq(newCount).show();
+  //   artViewArea.eq(beforeCount).fadeOut(function(){
+  //     artViewArea.eq(beforeCount).removeClass('act');
+  //     artViewArea.eq(newCount).addClass('act');
+  //     beforeCount = newCount;
+  //   });
+
+  //   setInterval(slideFadeFn(newCount), timed);
+  // };
+
 
 
 
